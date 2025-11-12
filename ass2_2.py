f3c4747f28747ec558579a31446d175d235190c1
Kalyani Nawale <kalyaninawale036@gmail.com>
2025-11-12 16:16:01 +0530

    first commit

---
+++ ass2_2.py
@@ -0,0 +1,8 @@
+
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+city = input("Enter your city: ")
+
+print(f"Hello, my name is {name}. I am {age} years old and I live in {city}.")
