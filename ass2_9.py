df1ab69d0268a121f681616e75c4c789baa9262d
Kalyani Nawale <kalyaninawale036@gmail.com>
2025-11-15 17:58:57 +0530

    first commit

---
+++ ass2_9.py
@@ -0,0 +1,11 @@
+# Get the first name from the user
+first_name = input("Enter your first name: ")
+
+# Get the last name from the user
+last_name = input("Enter your last name: ")
+
+# Combine the first and last names to create the full name
+full_name = first_name + " " + last_name
+
+# Display the full name
+print("Your full name is:", full_name)
